6c16c5784a911104ab8d73d8cf0b1738f4126ddd
Sass Bálint <sass.balint@nytud.mta.hu>
2018-12-21 00:15:41 +0100

    m adatok 500 helyett 1200-ig

--- bolg/jani-szamsora/data2html.sh
+++ bolg/jani-szamsora/data2html.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 DATA=$1
 
-cat $DATA | head  -25 | tail -25 > a
-cat $DATA | head  -50 | tail -25 > b
-cat $DATA | head  -75 | tail -25 > c
-cat $DATA | head -100 | tail -25 > d
-cat $DATA | head -125 | tail -25 > e
-cat $DATA | head -150 | tail -25 > f
-cat $DATA | head -175 | tail -25 > g
-cat $DATA | head -200 | tail -25 > h
-cat $DATA | head -225 | tail -25 > i
-cat $DATA | head -250 | tail -25 > j
-cat $DATA | head -275 | tail -25 > k
-cat $DATA | head -300 | tail -25 > l
-cat $DATA | head -325 | tail -25 > m
-cat $DATA | head -350 | tail -25 > n
-cat $DATA | head -375 | tail -25 > o
-cat $DATA | head -400 | tail -25 > p
-cat $DATA | head -425 | tail -25 > q
-cat $DATA | head -450 | tail -25 > r
-cat $DATA | head -475 | tail -25 > s
-cat $DATA | head -550 | tail -25 > t
+cat $DATA | head   -60 | tail -60 > a
+cat $DATA | head  -120 | tail -60 > b
+cat $DATA | head  -180 | tail -60 > c
+cat $DATA | head  -240 | tail -60 > d
+cat $DATA | head  -300 | tail -60 > e
+cat $DATA | head  -360 | tail -60 > f
+cat $DATA | head  -420 | tail -60 > g
+cat $DATA | head  -480 | tail -60 > h
+cat $DATA | head  -540 | tail -60 > i
+cat $DATA | head  -600 | tail -60 > j
+cat $DATA | head  -660 | tail -60 > k
+cat $DATA | head  -720 | tail -60 > l
+cat $DATA | head  -780 | tail -60 > m
+cat $DATA | head  -840 | tail -60 > n
+cat $DATA | head  -900 | tail -60 > o
+cat $DATA | head  -960 | tail -60 > p
+cat $DATA | head -1020 | tail -60 > q
+cat $DATA | head -1080 | tail -60 > r
+cat $DATA | head -1140 | tail -60 > s
+cat $DATA | head -1200 | tail -60 > t
 
 echo "<table style=\"font-size: smaller\">"
 paste a b c d e f g h i j k l m n o p q r s t | \
