3ad7f6374be199493451f5a780f2db34d17f23ba
Sass Bálint <sass.balint@nytud.mta.hu>
2019-04-01 11:52:13 +0200

    2019. május

--- pasa_ebed/pasa_ebed_config.js
+++ pasa_ebed/pasa_ebed_config.js
@@ -1,8 +1,8 @@
 // <HONAPROL_HONAPRA_FRISSITENDO>
 //  * aktuális év és hónap szöveggel
-var evho = '2019. április';
+var evho = '2019. május';
 //  * napok száma az aktuális hónapban
-var napokszama = 15;
+var napokszama = 21;
 // </HONAPROL_HONAPRA_FRISSITENDO>
 
 var ar = new Array();
