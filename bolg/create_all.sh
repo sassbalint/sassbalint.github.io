ba1d94492370f2b4383636eba1b73862077f87d8
Sass Bálint <sass.balint@nytud.mta.hu>
2019-11-25 23:18:34 +0100

    sorszámot bele -- folyamatban

--- bolg/create_all.sh
+++ bolg/create_all.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # TODO
-# XXX sorszámot bele
+# XXX sorszámot bele -- folyamatban :)
 # XXX linkeket automatizálni
 # XXX hashtag-et => data
 # XXX hashtag-eket kezelni, kiírni
 
 for n in $(seq 1 $(cat data | wc -l))
 do
-  N=$(head -$n data | tail -1 | cut -f 2)
-  T=$(head -$n data | tail -1 | cut -f 3)
-  echo "$T @ sassbalint.github.io/BOLG" > tit
+  C=$(head -$n data | tail -1 | cut -f 1) # sorszám
+  N=$(head -$n data | tail -1 | cut -f 2) # fájlnév
+  T=$(head -$n data | tail -1 | cut -f 3) # szöveges cím
+  echo "#$C $T @ sassbalint.github.io/BOLG" > tit
   make ${N}.html
   rm -f tit
 done
