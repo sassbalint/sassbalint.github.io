2b1500de747ac451223b6c1cc948872fd3b675ff
Sass Bálint <sass.balint@nytud.mta.hu>
2020-03-02 11:19:43 +0100

    2020. április

--- pasa_ebed/pasa_ebed_config.js
+++ pasa_ebed/pasa_ebed_config.js
@@ -1,8 +1,8 @@
 // <HONAPROL_HONAPRA_FRISSITENDO>
 //  * aktuális év és hónap szöveggel
-var evho = '2020. március';
+var evho = '2020. április';
 //  * napok száma az aktuális hónapban
-var napokszama = 20;
+var napokszama = 16;
 // </HONAPROL_HONAPRA_FRISSITENDO>
 
 var ar = new Array();
